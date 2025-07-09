c0908e90778c51766f30b9739bf337809335ccf3
Aksiome <54895777+aksiome@users.noreply.github.com>
2025-07-09 23:01:14 +0200

    Fix removed collision_shape

--- src/main/java/net/gunivers/bookshelf/VoxelShape.java
+++ src/main/java/net/gunivers/bookshelf/VoxelShape.java
@@ -2,40 +2,23 @@ package net.gunivers.bookshelf;
 
 import com.google.gson.JsonArray;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 
 public class VoxelShape {
 
-    public List<AABB> shape;
+    public final List<AABB> shape;
 
     private static final int GRID_RESOLUTION = 64;
-    private boolean[][][] voxelGrid = new boolean[GRID_RESOLUTION][GRID_RESOLUTION][GRID_RESOLUTION];
+    private static final double INV_GRID_RESOLUTION = 1.0 / GRID_RESOLUTION;
+    private static final ConcurrentHashMap<VoxelShape, VoxelShape> cache = new ConcurrentHashMap<>();
 
-    public class AABB {
-        double minX, minY, minZ, maxX, maxY, maxZ;
-
-        public AABB(double minX, double minY, double minZ, double maxX, double maxY, double maxZ) {
-            this.minX = minX;
-            this.minY = minY;
-            this.minZ = minZ;
-            this.maxX = maxX;
-            this.maxY = maxY;
-            this.maxZ = maxZ;
-        }
-
+    public record AABB(double minX, double minY, double minZ, double maxX, double maxY, double maxZ) {
         public AABB(net.minecraft.world.phys.AABB box) {
-            this.minX = box.minX;
-            this.minY = box.minY;
-            this.minZ = box.minZ;
-            this.maxX = box.maxX;
-            this.maxY = box.maxY;
-            this.maxZ = box.maxZ;
+            this(box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ);
         }
 
         public JsonArray toJson() {
@@ -48,31 +31,24 @@ public class VoxelShape {
             box.add(this.maxZ);
             return box;
         }
-
-        public void fillVoxelGrid(boolean[][][] grid) {
-            int minX = Math.max((int) (this.minX * GRID_RESOLUTION), 0);
-            int minY = Math.max((int) (this.minY * GRID_RESOLUTION), 0);
-            int minZ = Math.max((int) (this.minZ * GRID_RESOLUTION), 0);
-            int maxX = Math.min((int) (this.maxX * GRID_RESOLUTION), GRID_RESOLUTION);
-            int maxY = Math.min((int) (this.maxY * GRID_RESOLUTION), GRID_RESOLUTION);
-            int maxZ = Math.min((int) (this.maxZ * GRID_RESOLUTION), GRID_RESOLUTION);
-
-            for (int x = minX; x < maxX; x++) {
-                for (int y = minY; y < maxY; y++) {
-                    for (int z = minZ; z < maxZ; z++) {
-                        grid[x][y][z] = true;
-                    }
-                }
-            }
-        }
     }
 
     public VoxelShape(List<AABB> shape) {
-        this.shape = shape;
+        this.shape = List.copyOf(shape);
     }
 
     public VoxelShape(net.minecraft.world.phys.shapes.VoxelShape shape) {
         this.shape = shape.toAabbs().stream().map(AABB::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shape);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof VoxelShape other && shape.equals(other.shape);
     }
 
     public JsonArray toJson() {
@@ -82,32 +58,32 @@ public class VoxelShape {
     }
 
     public VoxelShape optimize() {
-        for (AABB box : this.shape) {
-            box.fillVoxelGrid(this.voxelGrid);
-        }
-        
-        List<AABB> shape = new ArrayList<>();
-        boolean[][][] visited = new boolean[GRID_RESOLUTION][GRID_RESOLUTION][GRID_RESOLUTION];
+        return cache.computeIfAbsent(this, unused -> {
+            boolean[][][] voxels = new boolean[GRID_RESOLUTION][GRID_RESOLUTION][GRID_RESOLUTION];
+            boolean[][][] visited = new boolean[GRID_RESOLUTION][GRID_RESOLUTION][GRID_RESOLUTION];
+            for (AABB box : shape) fillVolume(voxels, box);
 
-        for (int x = 0; x < GRID_RESOLUTION; x++) {
-            for (int y = 0; y < GRID_RESOLUTION; y++) {
-                for (int z = 0; z < GRID_RESOLUTION; z++) {
-                    if (voxelGrid[x][y][z] && !visited[x][y][z]) {
-                        shape.add(findLargestAABB(x, y, z, visited));
+            List<AABB> shape = new ArrayList<>();
+            for (int x = 0; x < GRID_RESOLUTION; x++) {
+                for (int y = 0; y < GRID_RESOLUTION; y++) {
+                    for (int z = 0; z < GRID_RESOLUTION; z++) {
+                        if (voxels[x][y][z] && !visited[x][y][z]) {
+                            shape.add(greedyMergeAABB(voxels, visited, x, y, z));
+                        }
                     }
                 }
             }
-        }
-        return new VoxelShape(shape);
+            return new VoxelShape(shape);
+        });
     }
 
-    private AABB findLargestAABB(int startX, int startY, int startZ, boolean[][][] visited) {
+    private AABB greedyMergeAABB(boolean[][][] voxels, boolean[][][] visited, int startX, int startY, int startZ) {
         int maxX = startX;
         int maxY = startY;
         int maxZ = startZ;
 
         for (int y = startY + 1; y < GRID_RESOLUTION; y++) {
-            if (isVolumeFilled(startX, startY, startZ, maxX, y, maxZ)) {
+            if (isVolumeFilled(voxels, startX, startY, startZ, maxX, y, maxZ)) {
                 maxY = y;
             } else {
                 break;
@@ -115,7 +91,7 @@ public class VoxelShape {
         }
 
         for (int x = startX + 1; x < GRID_RESOLUTION; x++) {
-            if (isVolumeFilled(startX, startY, startZ, x, maxY, maxZ)) {
+            if (isVolumeFilled(voxels, startX, startY, startZ, x, maxY, maxZ)) {
                 maxX = x;
             } else {
                 break;
@@ -123,7 +99,7 @@ public class VoxelShape {
         }
 
         for (int z = startZ + 1; z < GRID_RESOLUTION; z++) {
-            if (isVolumeFilled(startX, startY, startZ, maxX, maxY, z)) {
+            if (isVolumeFilled(voxels, startX, startY, startZ, maxX, maxY, z)) {
                 maxZ = z;
             } else {
                 break;
@@ -139,20 +115,37 @@ public class VoxelShape {
         }
 
         return new AABB(
-            (double) startX / GRID_RESOLUTION,
-            (double) startY / GRID_RESOLUTION,
-            (double) startZ / GRID_RESOLUTION,
-            (double) (maxX + 1) / GRID_RESOLUTION,
-            (double) (maxY + 1) / GRID_RESOLUTION,
-            (double) (maxZ + 1) / GRID_RESOLUTION
+            (double) startX * INV_GRID_RESOLUTION,
+            (double) startY * INV_GRID_RESOLUTION,
+            (double) startZ * INV_GRID_RESOLUTION,
+            (double) (maxX + 1) * INV_GRID_RESOLUTION,
+            (double) (maxY + 1) * INV_GRID_RESOLUTION,
+            (double) (maxZ + 1) * INV_GRID_RESOLUTION
         );
     }
 
-    private boolean isVolumeFilled(int startX, int startY, int startZ, int maxX, int maxY, int maxZ) {
+    private void fillVolume(boolean[][][] voxels, AABB box) {
+        int minX = Math.max((int) (box.minX * GRID_RESOLUTION), 0);
+        int minY = Math.max((int) (box.minY * GRID_RESOLUTION), 0);
+        int minZ = Math.max((int) (box.minZ * GRID_RESOLUTION), 0);
+        int maxX = Math.min((int) (box.maxX * GRID_RESOLUTION), GRID_RESOLUTION);
+        int maxY = Math.min((int) (box.maxY * GRID_RESOLUTION), GRID_RESOLUTION);
+        int maxZ = Math.min((int) (box.maxZ * GRID_RESOLUTION), GRID_RESOLUTION);
+
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                for (int z = minZ; z < maxZ; z++) {
+                    voxels[x][y][z] = true;
+                }
+            }
+        }
+    }
+
+    private boolean isVolumeFilled(boolean[][][] voxels, int startX, int startY, int startZ, int maxX, int maxY, int maxZ) {
         for (int x = startX; x <= maxX; x++) {
             for (int y = startY; y <= maxY; y++) {
                 for (int z = startZ; z <= maxZ; z++) {
-                    if (!voxelGrid[x][y][z]) {
+                    if (!voxels[x][y][z]) {
                         return false;
                     }
                 }
